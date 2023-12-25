9352d2de25c911df7a08e95106254d3ff8b460f8
lingar <yimprogramming@gmail.com>
2023-12-25 10:45:58 +0200

    Adding inventory

---
+++ c-sharp-basics-lingar/inventory-module/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_basics_lingar.inventory_module
+{
+    internal class Inventory
+    {
+    }
+}
