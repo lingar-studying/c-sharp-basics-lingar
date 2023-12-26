6fe6345429d32a66ff3c0254f5c70d03da4b6fd5
lingar <yimprogramming@gmail.com>
2023-12-27 00:12:15 +0200

    Saving work

--- c-sharp-basics-lingar/RunMe.cs
+++ c-sharp-basics-lingar/RunMe.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using c_sharp_basics_lingar.inventory_module;
 namespace c_sharp_basics_lingar
+
 {
     public class RunMe
     {
@@ -12,6 +13,13 @@ namespace c_sharp_basics_lingar
         {
 
             Console.WriteLine("Some OOP examples cames here");
+            Inventory  inventory=new Inventory();
+
+           
+            //Console.WriteLine("id after change - " + inventory.id);//---> will generate an inaccessible error
+
+
+
 
         }
         //public static void Main(string v, string[] args)
--- c-sharp-basics-lingar/inventory-module/Inventory.cs
+++ c-sharp-basics-lingar/inventory-module/Inventory.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace c_sharp_basics_lingar.inventory_module
 {
-    internal class Inventory
+    public class Inventory
     {
+        private long id;
+        private string companyName;
+
+        public string CompanyName { get; set; }
+        public int MyProperty { get; set; }
+
+        //getter written as the field with capitalized Case
+        public long Id()
+        {
+            return id;
+        }
+
+        public override string ToString() { 
+        
+            return "Inventory of company - " + companyName + "id number = " + id;
+        }
+
+
+
+        //TODO - demo advanced getters and setters 
+        //public long Id2 { get; set; }
+    }
+
+    internal class OnlyInThisFile
+    {
+        int x = 0;
     }
 }
