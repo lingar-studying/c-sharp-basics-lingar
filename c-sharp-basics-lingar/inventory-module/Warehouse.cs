8467031cea2dd78ec2b213a3c3beb3131db2f2d1
lingar <yimprogramming@gmail.com>
2023-12-27 15:28:05 +0200

    Create warehouse child with light on

---
+++ c-sharp-basics-lingar/inventory-module/Warehouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_basics_lingar.inventory_module
+{
+    public class Warehouse: Structure
+    {
+        private bool lightOn = false;
+
+        private string[] keepers;
+
+        public bool LightOn { get { return lightOn; } set { lightOn = value; } }    
+    }
+}
