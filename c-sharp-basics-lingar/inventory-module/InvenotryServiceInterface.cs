ed382db27027ec047692d86ff523f7541b5263de
lingar <yimprogramming@gmail.com>
2023-12-28 17:50:53 +0200

    Adding interface Inventory Service

---
+++ c-sharp-basics-lingar/inventory-module/InvenotryServiceInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_basics_lingar.inventory_module
+{
+    public interface InvenotryServiceInterface
+    {
+        public void TransferProducts(Warehouse from, Warehouse to, int productBarcode, int amount); 
+    }
+}
