41bb3f3c4e463ca38389b9115e8f2890ff2f3818
lingar <yimprogramming@gmail.com>
2024-01-03 23:26:19 +0200

    Answer B - store

--- c-sharp-basics-lingar/test-summer-81/pickup-store-Q1/PickupStore.cs
+++ c-sharp-basics-lingar/test-summer-81/pickup-store-Q1/PickupStore.cs
@@ -12,6 +12,7 @@ namespace c_sharp_basics_lingar.test_summer_81.pickup_store_Q1
 
         private string name = String.Empty;
         private string kind = String.Empty;
+        /* W - windows A - android I - ios*/
         private char system;
         private double size ;
         private double price;
@@ -38,10 +39,16 @@ namespace c_sharp_basics_lingar.test_summer_81.pickup_store_Q1
         {
 
             //if name, kind, size equals - true
-            if (this.name.Equals( other.name) && this.kind.Equals(other.kind) && this.size == other.Size)  return true;
+            if (this.name.Equals( other.name) && this.kind.Equals(other.kind) && this.size == other.Size
+                
+                && this.System == other.System)//I assume it's checking also system... 
+                
+                return true;
 
             return false;
         }
+
+       
 
         public override string ToString()
         {
@@ -49,17 +56,76 @@ namespace c_sharp_basics_lingar.test_summer_81.pickup_store_Q1
                 "size = {3}| price = {4}]", name, kind, system, size, price);
         }
     }
+
+    public class Store
+    {
+        private Tablet[] tablets;
+        // 0 - W , 1-A, 2-I (W - windows A - android I - ios)
+
+        private int[] systems;
+        public Store()
+        {
+            tablets = new Tablet[4];
+            systems = new int[3];
+        }
+
+        public bool AddTablet(Tablet tab)
+        {
+            //bool storageFull = true;
+            int freeIndex = -1;
+            for(int i = 0; i < tablets.Length; i++)
+            {
+
+                //if no more room - return false;
+                //null should be on the end so the other condition should be catch before 
+                if (tablets[i] == null)
+                {
+                    //storageFull = false;
+                    freeIndex = i;
+                    break;
+                }
+                
+                //if exists update to the higher price
+                else if (tablets[i].IsSame(tab))
+                {
+                    tablets[i].Price = Math.Max(tablets[i].Price, tab.Price);
+                    return true;
+                }
+               
+                
+            }
+            //if you reach here this says it's a new item
+            if (freeIndex < 0) return false;
+
+            else{
+                tablets[freeIndex] = tab;
+                switch (tablets[freeIndex].System)
+                {
+                    case 'W':
+                        systems[0]++;
+                        break;
+                    case 'A':
+                        systems[1]++;
+                        break;
+                    case 'I':
+                        systems[2]++;
+                        break;
+
+                }
+                return true;
+            }
+
+
+
+
+        }
+        public override string ToString()
+        {
+            return "Store = [Tablets:\n" + string.Join("| ", (object[]) tablets) 
+                + "\nSystems = W - " + systems[0] + " | A - " + systems[1] + " | I = " + systems[2] + "]"; //- the way to print array easily
+
+        }
+        //some code come here
+    }
 }
 
-/**
- * המחלקה Tablet המייצגת מחשב הלוח. תכונות המחלקה:
-name  – שם חברה המייצרת, מסוג מחרוזת, stringkind  – דגם, מסוג מחרוזת, stringsystem  – מערכת הפעלה מסוג תו, char '( W ' – ווינדוס, ' A ' – אנדרואיד, ' I – ' iOS )
-size  – גודל, מסוג מספר ממשי, doubleprice  – מחיר, מסוג מספר ממשי, double
-אפשר להניח שבמחלקה הוגדר בנאי ) constructor ( המקבל פרמטרים לכל התכונות, פעולות get/set
-ופעולת tostring . אין צורך לממש את הפעולות.
- * 
- * 
- * 
- * 
- * 
- * */
--- c-sharp-basics-lingar/test-summer-81/pickup-store-Q1/TestPickupStore.cs
+++ c-sharp-basics-lingar/test-summer-81/pickup-store-Q1/TestPickupStore.cs
@@ -13,11 +13,40 @@ namespace c_sharp_basics_lingar.test_summer_81.pickup_store_Q1
         {
             Tablet t = new Tablet("lingar", "5782", 'W', 10.7, 2004.64 );
             Tablet t2 = t;
-            Tablet t3 = new Tablet("lingar", "5782", 'A', 10.7, 2004.64);
+            Tablet t3 = new Tablet("lingar", "5782", 'I', 10.7, 2004.64);
+
+
+            Tablet t4 = new Tablet("lingar", "5782", 'W', 10.7, 2224.64);
+
+            Tablet t5 = new Tablet("lingarV3", "5782", 'A', 10.7, 2004.64);
+
+            Tablet t6 = new Tablet("lingarV3", "5782", 'A', 10.7, 2004.64);
+
 
             Console.WriteLine("Tablet t = " );
 
             Console.WriteLine("IS same ? - " + t.IsSame(t3));
+
+            Store store = new Store();
+
+            store.AddTablet(t);
+
+            Console.WriteLine("Store = " + store);
+
+            store.AddTablet(t3);
+
+            Console.WriteLine("Store = " + store);
+
+            store.AddTablet(t4);//only update price to t1. 
+
+            Console.WriteLine("Store = " + store);
+
+
+            store.AddTablet(t5);//only update price to t1. 
+
+            store.AddTablet(t6);//only update price to t1. 
+
+            Console.WriteLine("Store = " + store);
 
 
         }
