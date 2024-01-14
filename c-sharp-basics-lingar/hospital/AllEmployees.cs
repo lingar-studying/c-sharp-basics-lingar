1a4b4cc2acdc80fbb94f98cb74a89f2df50a700b
lingar <yimprogramming@gmail.com>
2024-01-14 02:58:51 +0200

    UML basics

---
+++ c-sharp-basics-lingar/hospital/AllEmployees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_basics_lingar.hospital
+{
+    public class AllEmployees
+    {
+
+
+    }
+}
