80a293f58e46f3a182ba6b038e8a81d369a944ca
yim222 <yimprogramming@gmail.com>
2024-01-19 12:03:49 +0200

    Some changes

---
+++ c-sharp-basics-lingar/chess/NotSmartImplementation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_basics_lingar.chess
+{
+    //each piece separately
+    class NotSmartImplementation
+    {
+        public static void Demo()
+        {
+
+        }
+    }
+
+    //write the things
+   
+
+}
