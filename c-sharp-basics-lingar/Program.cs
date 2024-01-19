13f3fd98ac1ca2a347bd923281086e5b320a5f8d
yim222 <yimprogramming@gmail.com>
2024-01-19 12:27:34 +0200

    Adding pieces

--- c-sharp-basics-lingar/Program.cs
+++ c-sharp-basics-lingar/Program.cs
@@ -6,6 +6,8 @@ using c_sharp_basics_lingar.hospital;
 using c_sharp_basics_lingar.polymorphism;
 using System;
 using c_sharp_basics_lingar.test_solution;
+using c_sharp_basics_lingar.chess;
+
 namespace c_sharp_basics_lingar
 {
     public class Program
@@ -18,8 +20,12 @@ namespace c_sharp_basics_lingar
             Console.WriteLine("By the help of God -" +
             "" +
             "Some C# basics");
-            Second s = new Second();
-            Console.WriteLine("second = " + s);
+
+            NotSmartImplementation.Demo();
+            NotSmartImplementation n = new NotSmartImplementation();
+            //n.Demo();
+            //Second s = new Second();
+            //Console.WriteLine("second = " + s);
             /*
              * We'll do here some tasks for demo and learn the basics of C# and OOP
              * 
@@ -30,7 +36,8 @@ namespace c_sharp_basics_lingar
             //TestPickupStore.runMe();
             //TestHospital.Test1();
             //TestHospital.Test2();
-            AbstractDemo.DemoAbstractClass();
+            //AbstractDemo.DemoAbstractClass();
+           
 
         }
     }
--- c-sharp-basics-lingar/chess/NotSmartImplementation.cs
+++ c-sharp-basics-lingar/chess/NotSmartImplementation.cs
@@ -9,11 +9,116 @@ namespace c_sharp_basics_lingar.chess
     {
         public static void Demo()
         {
+            Square sqaure = new Square();
+            sqaure.Column = 10;
+            Console.WriteLine(sqaure.Column);
 
+            sqaure.Column =5;
+            Console.WriteLine(sqaure.Column);
         }
     }
 
     //write the things
-   
+    public class Square
+    {
+        private int column = 1;
+        private int row = 1;
+
+        public int Column
+        {
+            get => column; set
+            {
+                if (value > 0 && value < 9)
+                {
+                    column = value;
+                }
+            }
+        }
+        public int Row
+        {
+            get => row; set
+            {
+                if (value > 0 && value < 9)
+                {
+                    row = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return "column=" + column + "row = " + row;
+        }
+    }
+    public class Board
+    {
+
+    }
+
+
+    class Queen
+    {
+        
+        private string color;
+        private Square sqaure;
+        // constractor
+        public Queen(string color)
+        {
+            this.color = color;
+            this.sqaure = sqaure;
+        }
+        public void Move(Square sqaure)
+        {
+            Console.WriteLine("The queen moved to square " + sqaure);
+        }
+    }
+
+    class Rook
+    {
+
+        private string color;
+        private Square square;
+        // constractor
+        public Rook(string color, Square square)
+        {
+            this.color = color;
+            this.square = square;
+        }
+        public void Move(Square sqaure)
+        {
+            Console.WriteLine("The Rook moved to square " + sqaure);
+        }
+    }
+    class Bishop
+    {
+
+        private string color;
+        private Square square;
+        // constractor
+        public Bishop(string color, Square square)
+        {
+            this.color = color;
+            this.square = square;
+        }
+        public void Move(Square sqaure)
+        {
+            Console.WriteLine("The Bishop moved to square " + sqaure);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
