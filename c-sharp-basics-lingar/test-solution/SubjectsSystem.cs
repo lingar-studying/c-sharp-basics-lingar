5df7164bca28d8b8aa82997dfe5c8a38d60bf3a2
lingar <yimprogramming@gmail.com>
2024-02-01 17:48:23 +0200

    Section A - creating subject

---
+++ c-sharp-basics-lingar/test-solution/SubjectsSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_basics_lingar.test_solution
+{
+    public class SubjectsSystem
+    {
+    }
+
+    public class Subject
+    {
+        private int creditPoints;
+        private int assignments;
+        private int passingGrade;
+
+        public int GetCreditPoints() { return creditPoints; }
+
+        public int GetAssignments() { return assignments; }
+
+        public int GetPassingGrade() { return passingGrade; }
+
+        public Subject(int creditPoints, int assignments, int passingGrade)
+        {
+            this.creditPoints = creditPoints;
+            this.assignments = assignments;
+            this.passingGrade = passingGrade;
+        }
+    }
+}
