51df72a2427b49f66ef50d9e1c6cf473f38dd504
lingar <yimprogramming@gmail.com>
2024-02-01 21:11:53 +0200

    Saving... right solution

---
+++ c-sharp-basics-lingar/draft/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace College
+{
+    class Program
+    {
+        public static void result(Subject[] arr)
+        {
+            int sum = 0;
+            int task = int.Parse(Console.ReadLine());
+            int passesGr = int.Parse(Console.ReadLine()); 
+            for(int i=0; i<arr.Length; i++)
+            {
+                sum += arr[i].getPoints(task,passesGr);
+            }
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ c-sharp-basics-lingar/draft/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace College
+{
+    class Subject
+    {
+        private int task;
+        private int passesGr;
+        private int credits;
+
+        public Subject(int Task, int PassesGr, int Credits)
+        {
+            this.task = Task;
+            this.passesGr = PassesGr;
+            this.credits = Credits;
+        }
+
+        public int getPoints(int numOfWorks,int grade) 
+        {
+            if((numOfWorks==task)&&(grade>=passesGr))
+
+                   return credits;
+            else
+                   return 0;
+        }
+    }
+}
