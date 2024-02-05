951fe425a37b156ba132a38cd2f7fd728e0310ff
lingar <yimprogramming@gmail.com>
2024-02-05 17:57:37 +0200

    Inserting testing in comments

--- c-sharp-basics-lingar/test-solution/some-test-01/PaymentsSystem.cs
+++ c-sharp-basics-lingar/test-solution/some-test-01/PaymentsSystem.cs
@@ -9,17 +9,121 @@ namespace c_sharp_basics_lingar.test_solution.some_test_01
         public static void DemoPaymentsSystem()
         {
             Console.WriteLine("DemoPayementsSystem");
+
+
+            /*
+             *  לשלם את כל הסכום באמצעי תשלום אחד: מזומן, המחאה או כרטיס אשראי.
+ לשלם 200 ש"ח במזומן ו- 800 ש"ח בכרטיס אשראי בארבעה תשלומים שווים )כל חודש יחויב סכום של 200 ש"ח(.
+ לשלם 100 ₪ במזומן, 500 ₪ באמצעות שתי המחאות: הראשונה על סך 200 ₪ והשנייה על סך 300 ₪
+ו- 400 ₪ בכרטיס אשראי.
+            */
+            /*
+            Buy buy = new Buy();
+            Payment[] payments = new Payment[2];
+
+            Credit c = new Credit();
+            c.sum = 10;
+            c.serialNumber = "1000";
+
+            payments[0] = c;
+
+            c = new Credit();
+
+            c.sum = 100;
+            c.serialNumber = "1000";
+
+            payments[1] = c;
+
+            buy.payments = payments;
+
+            buy.total = 110;
+
+            Console.WriteLine("check() = true " + buy.Check());
+
+            buy.total = 100;
+
+            Console.WriteLine("check() = false " + buy.Check());
+
+            Buy buy2 = new Buy();
+
+
+
+            payments = new Payment[1];
+
+            payments[0] = new Cash();
+            payments[0].sum = 20;
+            buy2.payments = payments;
+
+            Store store = new Store();
+            store.buys = new Buy[]{buy, buy2 };
+            store.Print("1000");
+
+            Console.WriteLine("store cash count = " + store.CashPayments());
+
+
+            */
+
+
         }
     }
 
     public class Store
     {
         //הקניות שבוצעות בחנות, ההנחה שלא כולם בוצעו בהצלחה לכן עוקבים גם אחרי הכמות הנכונה
+        //buys has no nulls
         private Buy[] buys = new Buy[0];
 
         //כמות הקניות האמיתית.
         private int buysAmount = 0;
-    
+
+
+        public void Print(string creditNum)
+        {
+            for (int i = 0; i < buys.Length; i++)
+            {
+                //represent if this buy contains is Credit
+                bool isCredit = false;
+                for (int j = 0; j < buys[i].GetPayments().Length; j++)
+                {
+                    //for code clearence 
+                    Payment pay = buys[i].GetPayments()[j];
+                    if (pay is Credit && pay.GetSerialNumber().Equals(creditNum))
+                    {
+                        isCredit = true; break;
+                    }
+
+                }
+                if (isCredit)
+                {
+                    Console.WriteLine(buys[i]);
+                }
+            }
+        }
+
+
+        public int CashPayments()
+        {
+
+            int counter = 0;
+            for (int i = 0; i < buys.Length; i++)
+            {
+                for (int j = 0; j < buys[i].GetPayments().Length; j++)
+                {
+                    //for code clearence 
+                    Payment pay = buys[i].GetPayments()[j];
+
+                    //Checking that it's cash
+                    if (pay is Cash)
+                    {
+                        counter++;
+                    }
+
+                }
+            }
+            return counter;
+        }
+
+
     }
 
 
@@ -40,6 +144,8 @@ namespace c_sharp_basics_lingar.test_solution.some_test_01
 
         //הכמות הנכונה של מס' האמצעים ששימשו לתשלום הקנייה
         private int paymentsAmount = 0;
+        //For testing
+        public Payment[] GetPayments() { return payments; }
 
 
         public bool Check()
@@ -54,6 +160,13 @@ namespace c_sharp_basics_lingar.test_solution.some_test_01
             return sum == total;
         }
 
+        //for testing
+        //public override string ToString()
+        //{
+
+        //    return string.Format("buy total  = {0}", total);
+        //}
+
     }
 
     public class Payment
@@ -62,19 +175,26 @@ namespace c_sharp_basics_lingar.test_solution.some_test_01
         private double sum;
 
 
-       
+
         public double GetSum()
         {
 
             return sum;
         }
+
+
         //the serial number of the payment. Cash will be -1
         private string serialNumber = "-1";
+
+        public string GetSerialNumber() { return serialNumber; }
+
+       
     }
 
-    public class Cash : Payment   {    }
-    
-    public class Cheque : Payment {
+    public class Cash : Payment { }
+
+    public class Cheque : Payment
+    {
 
         //the name of the bank
         private string bankName = "";
@@ -85,7 +205,8 @@ namespace c_sharp_basics_lingar.test_solution.some_test_01
 
     }
 
-    public class Credit : Payment {
+    public class Credit : Payment
+    {
 
         //בעבור תשלום בכרטיס אשראי – הסכום לתשלום, מספר כרטיס האשראי, תוקף הכרטיס והתאריך שבו יחויב
 
