6ebf01a6850576af546b1a7cfff2ddf579876df7
yim222 <yimprogramming@gmail.com>
2024-02-22 19:24:04 +0200

    Initial commit

---
+++ izhar-algo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace izhar_algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
