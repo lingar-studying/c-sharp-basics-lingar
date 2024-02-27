5fcaf3ef966303fc8fa1276d05ad41b939b024b7
lingar <yimprogramming@gmail.com>
2024-02-27 02:32:55 +0200

    Saving

--- c-sharp-basics-lingar/test-solution/middle-test/MiddleTestQ4.cs
+++ c-sharp-basics-lingar/test-solution/middle-test/MiddleTestQ4.cs
@@ -9,6 +9,7 @@ namespace c_sharp_basics_lingar.test_solution.middle_test
         public static void Demo()
         {
             A a = new A();
+            B b = new B();
         }
     }
 
@@ -16,10 +17,10 @@ namespace c_sharp_basics_lingar.test_solution.middle_test
     {
 
         //Section A
-        public int Foo(int i)
-        {
-            return i;
-        }
+        //public int Foo(int i)
+        //{
+        //    return i;
+        //}
         // public int Foo() { return 1; }//OK - overloading
 
         //public int Foo(double i) { return (int)i; }//OK - overloading
@@ -30,7 +31,26 @@ namespace c_sharp_basics_lingar.test_solution.middle_test
 
         //public void Foo(double i) { Console.Write(i); }//OK - overloading
 
+        //Section B
+
+        public virtual int Foo(int i)
+        {
+            return i;
+        }
 
     }
+
+    public class B : A
+    {
+        //public int Foo() { return 10; }// OK - Overloading
+        //public override int Foo(int i) { return i * 10; }//OK - Overriding
+
+        //public override double Foo(int i) { return i * 10; }//Compilation
+
+        //public double Foo(double i) { return i * 10; }// OK - overloading
+
+        public override void Foo(double i) { Console.Write(i); }//Compilation 
+    }
+
 }
 
