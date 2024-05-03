899834170cff3e9b5c98f4755617347821b3cf6d
visual <visual@hermelin.ort.org.il>
2024-05-03 13:00:57 +0300

    saving

--- c-sharp-basics-lingar/fun/project-euler/EulerProblem47.cs
+++ c-sharp-basics-lingar/fun/project-euler/EulerProblem47.cs
@@ -11,16 +11,21 @@ Problem - find the first number of the sequence of four number with distinct fac
 Steps to solve: 
 *** all should be done in the fastest and efficient way ***
 *
--
+- Start from 644 (according to the question description)
 - Find the primes factors of numbers. 
 - On each number we need to compare it vs it's previous if it's distinct. 
 - if does - save the first. and  - count and check the next.
 - if you reach 4 - this is the number - return the first you have saved. 
 - if not distinct - reset the count. 
 - 
-
+Discussion 
   
-  
+  How to find factors - at all and in the fastest way. 
+    Start from 2 and check if it's divided. If does return this method on the result. 
+    if it's not - it's a prime
+    if it's odd - you can skip evens. 
+    If you reach half or more then half of the number - it won't be divided any more. 
+    
  * */
 namespace c_sharp_basics_lingar.fun.project_euler
 {
