34be026ef08b649d9cf9273e967a8dfe470aefc6
lingar <yimprogramming@gmail.com>
2024-05-13 18:12:05 +0300

    5-b

--- c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ5.cs
+++ c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ5.cs
@@ -6,6 +6,16 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
 {
     public class Magen24BQ5
     {
+
+        public static void Demo()
+        {
+            Shape s = new Square(new Point(50, 50), 10);
+            Shape c = new Circle(new Point(50, 50), 8);
+            ITurnable t;
+            IFillable f;
+            //***
+
+        }
     }
     public interface IFillable
     {
@@ -20,17 +30,31 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
     //סעיף א
     public class Shape
     {
-
+        
     }
 
     public class Circle : Shape, IFillable
     {
+        //סעיף ב
+        public Circle(Point point, int i)
+        {
+
+        }
         public void Fill(string color) { }
 
     }
 
     public class Square: Shape, IFillable, ITurnable
     {
+
+        //סעיף ב
+        //חשוב ליצור גם אחד ריק, כדי שמלבן לא יזרוק תקלה
+        //לחילופין אפשר גם ליצור אחד במלבן שמתאים לבנאי הזה. 
+        public Square() { }
+        public Square(Point point, int i)
+        {
+
+        }
         public void Fill(string color) { }
         public void Right(int degree) { }
 
@@ -44,6 +68,6 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
 
     public class Point
     {
-
+        public Point(int x, int y) { }
     }
 }
