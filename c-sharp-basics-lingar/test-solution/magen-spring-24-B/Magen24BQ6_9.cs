58ae266e66e8143ec19aa00c5bb14a1a69602793
lingar <yimprogramming@gmail.com>
2024-05-15 00:04:29 +0300

    Finished section A

--- c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ6_9.cs
+++ c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ6_9.cs
@@ -10,6 +10,13 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
         public static void Demo()
         {
             Console.WriteLine("Soccer Data app:");
+            Team barchelona = new Team();
+            barchelona.SetName("FC Barcelona");
+            barchelona.SetCity("Barcelona");
+
+            Console.WriteLine("Team = " + barchelona);
+
+
         }
 
 
@@ -40,8 +47,67 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
         private int goalsAgainst;
 
         //הפרש שערים
-        private int goalDifferential;      
+        private int goalDifferential;
+
+        //ctrl+r ctrl+e
+        public int GoalsFor { get => goalsFor; set => goalsFor = value; }
+
+        //constructors and getters and setters come here
+        //I did some things I need for demonstration
+
+        public void SetName(string name) { this.name = name; }
+        public void SetCity(string city) { this.city = city; }
+
+        public string GetCity() { return this.city; }
+
+        public override string ToString()
+        {
+            return string.Format("Team=[name={0}, city={1}]", name, city);
+        }
+
+
+
+
+    }
+
+    public class Game
+    {
+        //קבוצה א
+        private Team teamA;
+        //קבוצה ב
+        private Team teamB;
+
+        
+        //מספרי שערים
+        private int teamAGoals;
+        private int teamBGoals;
+
+        private int currentMinute;
+
+        private bool isActive;
+
+    }
+
+    public class Round
+    {
+        //אפשר גם בבנאי לתת את הערך
+        private Game[] games = new Game[6];
+    }
+
     
-    
+    public class Season
+    {
+        private int year;
+
+        private string league;
+
+        private int roundsAmount;
+
+        private int nextRound;
+
+        private Team[] teams= new Team[12];
+
+        private bool isActive;
+
     }
 }
