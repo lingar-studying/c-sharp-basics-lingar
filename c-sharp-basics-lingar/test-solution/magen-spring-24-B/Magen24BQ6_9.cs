f4acc87d9fff7443eb245ed0d493a249e8810812
lingar <yimprogramming@gmail.com>
2024-05-14 23:49:45 +0300

    Adding team

--- c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ6_9.cs
+++ c-sharp-basics-lingar/test-solution/magen-spring-24-B/Magen24BQ6_9.cs
@@ -11,5 +11,37 @@ namespace c_sharp_basics_lingar.test_solution.magen_spring_24_B
         {
             Console.WriteLine("Soccer Data app:");
         }
+
+
+    }
+
+    //קבוצה
+    public class Team
+    {
+        private string name;
+        private string city;
+        private string currentLeague;
+
+        //סהכ משחקים
+        public int totalGames;
+
+        //משחקים ששוחקו
+        private int gamesPlayed;
+        //נצחונות
+        public int wins;
+
+        //הפסדים
+        public int loses;
+
+        //שערי זכות
+        private int goalsFor;
+
+        //שערי חובה
+        private int goalsAgainst;
+
+        //הפרש שערים
+        private int goalDifferential;      
+    
+    
     }
 }
