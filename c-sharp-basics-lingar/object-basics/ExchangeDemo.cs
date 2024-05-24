780429d156046fcbc88e57c09f7c57180e45d20f
visual <visual@hermelin.ort.org.il>
2024-05-24 13:58:26 +0300

    Home work

---
+++ c-sharp-basics-lingar/object-basics/ExchangeDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//HOME WORK
+namespace c_sharp_basics_lingar.object_basics
+{
+    public class ExchangeDemo
+    {
+        //TODO - 
+        //Do demo method for 3 stocks ( =מנייה)
+        //Show start day price, close day price, percent change (=אחוזי שינוי)
+
+        //work on the details of the date: 05/21/2024	
+        //those are the details: 
+        //https://www.nasdaq.com/market-activity/stocks/aapl/historical
+        //https://www.nasdaq.com/market-activity/stocks/tsla/historical
+        //https://www.nasdaq.com/market-activity/stocks/coke/historical
+
+
+        //On each  stock:
+
+        //Do start day (according to the real time above) 
+        //Show details
+        //Set the price of the stock that was the higher on this day
+        //Show details
+        //Try to get the percent change and get rejected
+        //close the day
+        //Show details
+        //get the percent change
+    }
+
+
+    public class Stock
+    {
+        //create properties : 
+        //Official Name, price, sign (=סמליל) ,starting price , closing price, industry(=תעשייה), exchange (=בורסה)
+        // is day active 
+
+        //create methods (פעולות ) 
+        //start day - setting the current price and the starting price and day is active
+        //finish day - setting the current price and the closing price
+        //set price - setting the price
+        //Print changes percentage (if the day is still  active it's not return nothing) 
+        //Display Price - printing the Sign with the current price. 
+
+    }
+
+   
+
+}
