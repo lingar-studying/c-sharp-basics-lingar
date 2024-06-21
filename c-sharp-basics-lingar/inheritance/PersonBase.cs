8305090049a48b80a7fff383bb7d91a316abcb20
izhar <visual@hermelin.ort.org.il>
2024-06-21 13:42:02 +0300

    Polymorphism

---
+++ c-sharp-basics-lingar/inheritance/PersonBase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_basics_lingar.inheritance
+{
+    public class PersonBase
+    {
+        protected int strengh;
+        private string name;
+
+        public int GetStrengh()
+        {
+            return strengh;
+        }
+
+
+        public PersonBase(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void MaximumStrengh()
+        {
+            this.strengh = 40;
+        }
+
+        public override string ToString()
+        {
+            return $"Person, Strengh = {strengh}, name = {name}";
+        }
+
+    }
+
+    public class Hero : PersonBase
+    {
+        private string specialization;
+        public Hero(string name, string specialization) : base(name)
+        {
+            this.specialization = specialization;
+        }
+        public override void MaximumStrengh()
+        {
+            this.strengh = 100;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", specialization = {specialization}";
+        }
+
+        public void DoHero() { }
+    }
+
+    public class SuperHero : Hero
+    {
+        public SuperHero(string name, string specialization) : base(name, specialization)
+        {
+
+        }
+        public override void MaximumStrengh()
+        {
+            this.strengh = 1000;
+        }
+
+    }
+
+    public class Kid : PersonBase
+    {
+        private bool inGarden;
+        public Kid(string name, bool inGarden) : base(name)
+        {
+            this.inGarden = inGarden;
+        }
+
+        public override void MaximumStrengh()
+        {
+            if (inGarden)
+            {
+                this.strengh = 10;
+            }
+            else
+            {
+                this.strengh = 20;
+            }
+        }
+    }
+
+    public class Ball
+    {
+        public string color = "red";
+
+        public override string ToString()
+        {
+            //return base.ToString() + " some ball in color " + color;
+            return " some ball in color " + color;
+
+        }
+        public override bool Equals(object obj)
+        {
+            //if(obj.color.Equals())
+            return base.Equals(obj);
+        }
+    }
+
+
+
+
+}
--- c-sharp-basics-lingar/inheritance/TestInheritance.cs
+++ c-sharp-basics-lingar/inheritance/TestInheritance.cs
@@ -15,6 +15,72 @@ namespace c_sharp_basics_lingar.inheritance
             Car c2 = new Car(300, "Honda", 40);
             //c2.DisplayDetails();
             c2.DisplayCarDetails();
+
+
+            PersonBase regularPerson = new PersonBase("Regular Person");
+            Hero hero = new Hero("BirdMan", "can fly");
+            Console.WriteLine(regularPerson.GetStrengh());
+            regularPerson.MaximumStrengh();
+
+
+
+
+            Console.WriteLine(regularPerson.GetStrengh());
+
+            Console.WriteLine(hero.GetStrengh());
+            hero.MaximumStrengh();
+
+            Console.WriteLine(regularPerson);
+
+            Console.WriteLine(hero);
+
+            Console.WriteLine(hero.GetStrengh());
+            Ball ball123 = new Ball();
+            Console.WriteLine(ball123);
+            Hero hero2 = hero;
+            Hero hero3 = new Hero("BirdMan", "can fly");
+
+            Console.WriteLine("hero == hero2? " + hero.Equals(hero2));
+            Console.WriteLine("hero == hero3? " + hero.Equals(hero3));
+
+            PersonBase regularPerson2 = new PersonBase("Regular Person");
+            Hero hero4 = new Hero("BirdMan", "can fly");
+            SuperHero hero5 = new SuperHero("IceMan", "can freeze");
+            Kid kid1 = new Kid("Dani", false);
+            hero4.DoHero();
+            //polymorphism demo
+            PersonBase[] allObjectTogether = { regularPerson2, hero4, hero5, kid1 };
+
+            for(int i=0; i< allObjectTogether.Length;i++)
+            {
+
+                PersonBase p = allObjectTogether[i];
+                p.MaximumStrengh();
+                Console.WriteLine(p);
+                //p.DoHero();
+            }
+
+            PersonBase regularPerson3 = new PersonBase("Regular Person");
+            PersonBase hero7 = new Hero("BirdMan", "can fly");
+            PersonBase hero6 = new SuperHero("IceMan", "can freeze");
+            PersonBase kid2 = new Kid("Dani", false);
+           // hero7.DoHero();
+            ((Hero)hero7).DoHero();
+            DemoPoly(hero6);
+            Console.WriteLine(hero7);
+            DemoPoly(hero7);
+
+
+
+
+            //Console.WriteLine(regularPerson.name);
+            //Console.WriteLine(regularPerson.strengh);
+
+        }
+        public static void DemoPoly(PersonBase p)
+        {
+            p.MaximumStrengh();
+            Console.WriteLine("p= " + p);
         }
     }
 }
