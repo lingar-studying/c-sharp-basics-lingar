e01995ba9bd4efc2b940831db39931247d9e9629
lingar <yimprogramming@gmail.com>
2024-09-27 07:31:02 +0300

    Saving

---
+++ c-sharp-basics-lingar/test-frontali/TestFrontaliDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_basics_lingar.test_frontali
+{
+    public class TestFrontaliDemo
+    {
+        public static void Demo()
+        {
+            Console.WriteLine("TestFrontaliDemo");
+
+            //write here your code 
+            double d = 4.5;
+            int i = d;//fix
+
+
+
+        }
+    }
+}
